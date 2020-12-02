515d2f1e1e5ad34a4fe26fdc2fa95c6d4f4c59e6
itsmeacs01 <itsmeacs01@gmail.com>
2020-12-03 02:39:13 +0545

    added first question of sorting

---
+++ README.md
@@ -0,0 +1,12 @@
+<h1 align="center"> CODE KATAS </h1>
+
+```
+ Solving code challenges with JavaScript.
+```
+
+<h3 align='left'> Tasks </h3>
+<!-- Add Tasks Here -->
+
+- [ ] Sorting.
+
+<h5 align="center" > **** To be continue ... **** </h5>
---
+++ src/sorting.js
@@ -0,0 +1,18 @@
+/*
+
+Question: Sort the array according to the "content of the elements".
+
+Problems: 
+    sortIt([4, 1, 3]) ➞ [1, 3, 4]
+
+    sortIt([[4], [1], [3]]) ➞ [[1], [3], [4]]
+    
+    sortIt([4, [1], 3]) ➞ [[1], 3, 4]
+    
+    sortIt([[4], 1, [3]]) ➞ [1, [3], [4]]
+    
+    sortIt([[3], 4, [2], [5], 1, 6]) ➞ [1, [2], [3], 4, [5], 6]
+
+*/
+
+// code here :
